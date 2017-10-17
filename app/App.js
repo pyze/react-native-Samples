245d74019e7ef30750b9af5cb3e852b141321086
LinusKeith <linuskeithalphonso@gmail.com>
2017-10-17 12:42:25 +0530

    updated sample

--- app/App.js
+++ app/App.js
@@ -1,28 +1,64 @@
 import React from 'react';
 
-import Home from './components/Home';
-import Events from './components/Events';
+import{
+    View,
+    Text,
+    Button,
+    StyleSheet,
+    TextInput
+} from 'react-native';
 
-import {
-    Router,
-    Scene
-} from 'react-native-router-flux';
+import{
+    PyzeEvents
+} from 'pyze-sdk-react-native';
 
 import{
     Platform
 } from 'react-native';
 
 class App extends React.Component{
+    state={
+        eventName: ''
+    }
     render(){
         return(
-            <Router>
-                <Scene key="root" style={{paddingTop:Platform.OS == 'ios'?64:54}}>
-                    <Scene key='home' component={Home} title='Home' />
-                    <Scene key='events' component={Events} title='Events' />
-                </Scene>
-            </Router>
+            <View>
+                <Text style={styles.title} >
+                    Post Custom Event
+                </Text>
+                <View style = {styles.button}>
+                <TextInput style={{width: 300, height: 50}}  placeholder='Custom Event Name' onChangeText={(text)=> this.setState({
+                        eventName: text
+                    })} value = {this.state.eventName} />
+                </View>
+                <View style = {styles.button}>
+                <Button title='Post Custom Event' onPress={()=> this.postEvent() } />
+                </View>
+            </View>
         );
     }
+    postEvent(){
+        alert("Posted " + this.state.eventName);
+        PyzeEvents.postCustomEvent(this.state.eventName);
+    }
 }
+
+
+var styles = StyleSheet.create({
+    title:{
+        marginTop : 20,
+        marginLeft : 20,
+        marginBottom: 20,
+        fontSize  : 20,
+        fontWeight : 'bold',
+        textAlign: 'center'
+    },
+    button:{
+        marginTop:10,
+        marginBottom:10,
+        marginLeft:10,
+        marginRight:10
+    }
+});
 
 export default App;
